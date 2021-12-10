0799721f3607d7603e3954b06322ffcbdab21171
AminoffZ <mouiylus@gmail.com>
2021-12-10 13:07:51 +0200

    Cleaner

--- gradient.js
+++ gradient.js
@@ -1,45 +1,32 @@
 const gradient = [
-    [
-      0,
-      [180, 0, 0]
-    ],
-    [
-      45,
-      [180, 180, 0]
-    ],
-    [
-      75,
-      [0, 180, 0]
-    ],
-    [
-      100,
-      [0, 0, 180]
-    ]
+    [0,
+      [180, 0, 0]],     // Red
+    [45,
+      [180, 180, 0]],   // Yellow
+    [75,
+      [0, 180, 0]],     // Green
+    [100,
+      [0, 0, 180]]      // Blue
 ];
 
+// We get a color from the gradient based on input
 function getColor(pos) {
+    let color = [0, 0, 0];
     if(pos === 0) {
-        return [0,0,0];
+        return color;
     }
-    let color = [];
     for (let i=0; i<gradient.length; i++) {
         if (pos > gradient[i][0]) {
-        continue;
+            continue;
         } else {
             const firstColor = gradient[i-1][1];
             const secondColor = gradient[i][1];
             const ratio = (pos-gradient[i-1][0])/(gradient[i][0]-gradient[i-1][0]);
-            const difference = [
-                secondColor[0]-firstColor[0],
-                secondColor[1]-firstColor[1],
-                secondColor[2]-firstColor[2]
-            ];
-            color = [
-                Math.round(firstColor[0]+difference[0]*ratio),
-                Math.round(firstColor[1]+difference[1]*ratio),
-                Math.round(firstColor[2]+difference[2]*ratio)
-            ];
-        return color;
+            for (let j=0; j<color.length; j++) {
+                color[j] = Math.round(firstColor[j]+(secondColor[j]-firstColor[j])*ratio);
+            }
+        break;
         }
     }
+    return color;
 }
